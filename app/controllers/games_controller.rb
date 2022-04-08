96e14ee39877bf792f2dfa873d55949eeb3e4396
Denzel Braithwaite <braithwaite.d@hotmail.com>
2022-04-07 20:49:11 -0400

    saving work before live code. Working on preventing illegal moves on score page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,24 @@ class GamesController < ApplicationController
   end
 
   def new
-    alphabet = ("a".."z").to_a
+    @alphabet = ("a".."z").to_a
     @letters = []
-    10.times { @letters << alphabet.sample }
+    10.times { @letters << @alphabet.sample }
+    # raise
   end
 
   def score
-    raise
+    @guess = params[:"guess-word"]
+    @letters = params[:letters]
+    @text = "stop cheating?" unless valid_letters?
+    # raise
+  end
+
+  # private
+
+  def valid_letters?
+    @guess.chars.all? do |letter|
+      @letters.include?(letter)
+    end
   end
 end
